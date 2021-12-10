8204b88a772964df718d4e2053946ae5f45bbaa0
distoddard <daniel.stoddard@sinclair.edu>
2021-12-10 15:41:07 -0500

    created program files started with the Vehicle class Car inherits info from Vehicle and adds doors

--- Inheritance/car.h
+++ Inheritance/car.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+#include "vehicle.h"
+using namespace std;
+
+#ifndef Car_h
+#define Car_h
+
+class Car :public Vehicle {
+private:
+	int doors;
+public:
+	Car();
+	Car(int, string, int);
+	void setDoors(int);
+	int getDoors();
+	void displayInfo();
+};
+#endif // !Car_h
